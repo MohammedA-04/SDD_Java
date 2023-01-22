5c4a627bcc63b9778b1f14ab34c971bf8ac77ffa
MohammedA-04 <116843561+MohammedA-04@users.noreply.github.com>
2023-01-22 15:05:29 +0000

    Add files via upload

---
+++ Week 10/Act_1A.java
@@ -0,0 +1,31 @@
+public class Act_1A {
+    public static void main(String[] args) {
+
+        ///System.out.println("Welcome to Java");
+
+
+        ///System.out.print("Welcome to Java");
+
+
+        //System.println('Welcome to Java');
+
+
+        //System.out.println('Welcome to Java');
+
+
+        //System.out.print('Welcome to Java');
+       // int myVal = 9;                    System.out.println(myVal);
+       // float myFloatVal = 8.99f;         System.out.println(myFloatVal);
+       // byte myLetterVal = 'A';           System.out.println(myLetterVal);
+       // boolean myBoolVal = false;        System.out.println(myBoolVal);
+       // String myTextVal = "Hello World"; System.out.println(myTextVal);
+
+        /* Outputs Values
+         * 9
+        * 8.99
+        * 65
+        * false
+        * Hello World
+        * */
+    }
+}
